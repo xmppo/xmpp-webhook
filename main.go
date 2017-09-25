3a1fc29d0cda64e557c21047d5a6220ea3e37f56
Thomas Maier <contact@thomas-maier.net>
2017-09-25 10:54:06 +0200

    WiP

--- main.go
+++ main.go
@@ -13,11 +13,8 @@ func fatalOnErr(err error) {
 	}
 }
 
-func main() {
-	xi := os.Getenv("XMPP_ID")
-	xp := os.Getenv("XMPP_PASS")
-
-	jid, err := xmpp.ParseJID(xi)
+func xmppLogin(id string, pass string) *xmpp.XMPP {
+	jid, err := xmpp.ParseJID(id)
 	fatalOnErr(err)
 
 	addr, err := xmpp.HomeServerAddrs(jid)
@@ -26,13 +23,26 @@ func main() {
 	stream, err := xmpp.NewStream(addr[0], nil)
 	fatalOnErr(err)
 
-	client, err := xmpp.NewClientXMPP(stream, jid, xp, nil)
+	client, err := xmpp.NewClientXMPP(stream, jid, pass, nil)
 	fatalOnErr(err)
 
 	client.Out <- xmpp.Presence{}
 
+	return client
+}
+
+func main() {
+	xi := os.Getenv("XMPP_ID")
+	xp := os.Getenv("XMPP_PASS")
+
+	if len(xi) < 1 || len(xp) < 1 {
+		log.Fatal("XMPP_ID or XMPP_PASS not set")
+	}
+
+	xc := xmppLogin(xi, xp)
+
 	go func() {
-		for msg := range client.In {
+		for msg := range xc.In {
 			log.Printf("* recv: %v\n", msg)
 		}
 	}()
